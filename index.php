c7116cceb98a68630439f9daf13a8a05d45a9626
DushanSenadheera <you@example.com>
2022-11-08 00:35:20 +0530

    commit

--- index.php
+++ index.php
@@ -1,202 +1,240 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="keywords" content="software engineer portfolio, portfolio, portfolio web, software engineer portfolio, dushan senadheera, dushan senadheera portfolio, web development, app development, digital content, creator, developer">
-    <meta name="description" content="Hi, I am Dushan Senadheera and I am a Software Engineer and Designer">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Dushan Senadheera | Portfolio</title>
+  <meta charset="UTF-8">
+  <meta name="keywords"
+    content="software engineer portfolio, portfolio, portfolio web, software engineer portfolio, dushan senadheera, dushan senadheera portfolio, web development, app development, digital content, creator, developer">
+  <meta name="description" content="Hi, I am Dushan Senadheera and I am a Software Engineer and Designer">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Dushan Senadheera | Portfolio</title>
 </head>
+
 <body>
-    <header id="header">
-        <nav>
-            <div class="menu">
-                <h1>Dushan.S_</h1>
-                <img src="assets/menu-1767818.png" alt="" id="menu-logo">
-            </div>
-                <ul id="mobileLinks">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li>
-                        <a id="download-btn" href="assets/Dushan Senadheera.pdf" download="Dushan Senadheera CV">
-                            <button>Download CV</button>
-                        </a>
-                    </li>
-                </ul>
-            <a class="downloadCV" href="" download="Dushan Senadheera CV">
-                <button>Download CV</button>
-            </a>
-        </nav>
-    </header>
-    <main>
-        <div class="hero" id="home">
-            <div class="hero-content">
-                <div class="hero-description">
-                    <div class="mobile-hero-img">
-                        <img src="assets/bgtransperent.png" alt="">
-                    </div>
-                    <h3>
-                        <span>Hii!</span> I am <br>
-                        Dushan Senadheera
-                    </h3>
-                    <h4>
-                        I am a, <br> <span><h5>Software Engineer & Designer</h5></span>
-                    </h4>
-                    <div class="cta">
-                        <button>Contact</button>
-                    </div>
-                </div>
-            </div>
-            <div class="hero-img">
-                <img src="assets/bgtransperent.png" alt="hero image">
-            </div>
+  <header id="header">
+    <nav>
+      <div class="menu">
+        <h1>Dushan.S_</h1>
+        <img src="assets/menu-1767818.png" alt="" id="menu-logo">
+      </div>
+      <ul id="mobileLinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a id="download-btn" href="assets/Dushan Senadheera.pdf" download="Dushan Senadheera CV">
+            <button>Download CV</button>
+          </a>
+        </li>
+      </ul>
+      <a class="downloadCV" href="" download="Dushan Senadheera CV">
+        <button>Download CV</button>
+      </a>
+    </nav>
+  </header>
+  <main>
+    <div class="hero" id="home">
+      <div class="hero-content">
+        <div class="hero-description">
+          <div class="mobile-hero-img">
+            <img src="assets/bgtransperent.png" alt="">
+          </div>
+          <h3>
+            <span>Hii!</span> I am <br>
+            Dushan Senadheera
+          </h3>
+          <h4>
+            I am a, <br> <span>
+              <h5>Software Engineer & Designer</h5>
+            </span>
+          </h4>
+          <div class="cta">
+            <button>Contact</button>
+          </div>
         </div>
-        <div class="technologies">
-            <h3 class="title">Technologies & Tools</h3>
-            <div class="technology">
-                <img src="assets/react.png" alt="">
-                <img src="assets/nodejs.png" alt="">
-                <img src="assets/angular.png" alt="">
-                <img src="assets/flutter.png" alt="">
-                <img src="assets/amazon-aws.png" alt="">
-                <img src="assets/mongodb.png" alt="">
-                <img src="assets/dart.png" alt="">
-                <img src="assets/kotlin.png" alt="">
-                <img src="assets/arduino.png" alt="">
-                <img src="assets/c.png" alt="">
-                <img src="assets/csharp.png" alt="">
-                <img src="assets/java.png" alt="">
-                <img src="assets/python.png" alt="">
-                <img src="assets/html.png" alt="">
-                <img src="assets/css3.png" alt="">
-                <img src="assets/javascript.png" alt="">
-                <img src="assets/php.png" alt="">
-                <img src="assets/mysql.png" alt="">
-                <img src="assets/bootstrap.png" alt="">
-                <img src="assets/icons8-tailwind-css-256.png" alt="">
-                <img src="assets/jquery.png" alt="">
-                <img src="assets/json.png" alt="">
-                <img src="assets/adobe-illustrator.png" alt="">
-                <img src="assets/adobe-photoshop.png" alt="">
-                <img src="assets/adobe-xd.png" alt="">
-                <img src="assets/figma.png" alt="">
-                <img src="assets/canva.png" alt="">
-                <img src="assets/vscode.png" alt="">
-                <img src="assets/android-studio.png" alt="">
-                <img src="assets/icons8-pycharm-256.png" alt="">
-                <img src="assets/github.png" alt="">
-                <img src="assets/jira.png" alt="">
-                <img src="assets/slack.png" alt="">
-            </div>
+      </div>
+      <div class="hero-img">
+        <img src="assets/bgtransperent.png" alt="hero image">
+      </div>
+    </div>
+    <div class="technologies">
+      <h3 class="title">Technologies & Tools</h3>
+      <div class="technology">
+        <img src="assets/react.png" alt="">
+        <img src="assets/nodejs.png" alt="">
+        <img src="assets/angular.png" alt="">
+        <img src="assets/flutter.png" alt="">
+        <img src="assets/amazon-aws.png" alt="">
+        <img src="assets/mongodb.png" alt="">
+        <img src="assets/dart.png" alt="">
+        <img src="assets/kotlin.png" alt="">
+        <img src="assets/arduino.png" alt="">
+        <img src="assets/c.png" alt="">
+        <img src="assets/csharp.png" alt="">
+        <img src="assets/java.png" alt="">
+        <img src="assets/python.png" alt="">
+        <img src="assets/html.png" alt="">
+        <img src="assets/css3.png" alt="">
+        <img src="assets/javascript.png" alt="">
+        <img src="assets/php.png" alt="">
+        <img src="assets/mysql.png" alt="">
+        <img src="assets/bootstrap.png" alt="">
+        <img src="assets/icons8-tailwind-css-256.png" alt="">
+        <img src="assets/jquery.png" alt="">
+        <img src="assets/json.png" alt="">
+        <img src="assets/adobe-illustrator.png" alt="">
+        <img src="assets/adobe-photoshop.png" alt="">
+        <img src="assets/adobe-xd.png" alt="">
+        <img src="assets/figma.png" alt="">
+        <img src="assets/canva.png" alt="">
+        <img src="assets/vscode.png" alt="">
+        <img src="assets/android-studio.png" alt="">
+        <img src="assets/icons8-pycharm-256.png" alt="">
+        <img src="assets/github.png" alt="">
+        <img src="assets/jira.png" alt="">
+        <img src="assets/slack.png" alt="">
+      </div>
+    </div>
+    <h3 class="title" id="services">Services</h3>
+    <div class="services">
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/web dev.png" alt="web developer">
+            <h4>Web development & design</h4>
+          </div>
+          <div class="flip-card-back">
+            <p>
+              I will develop full stack
+              web application using
+              ReactJS and nodeJS
+            </p>
+          </div>
         </div>
-        <h3 class="title" id="services">Services</h3>
-        <div class="services">
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img src="assets/web dev.png" alt="web developer">
-                    <h4>Web development & design</h4>
-                  </div>
-                  <div class="flip-card-back">
-                    <p>
-                        I will develop full stack 
-                        web application using 
-                        ReactJS and nodeJS
-                    </p>
-                  </div>
-                </div>
-              </div> 
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img src="assets/app dev.png" alt="mobile app developer">
-                    <h4>Mobile app development & design</h4>
-                  </div>
-                  <div class="flip-card-back">
-                    <p>
-                       I will develop cross platform
-                       mobile application using 
-                       Flutter
-                    </p>
-                  </div>
-                </div>
-              </div> 
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img src="assets/dev.png" alt="developer">
-                    <h4>Desktop application development</h4>
-                  </div>
-                  <div class="flip-card-back">
-                    <p>
-                       I will develop desktop
-                       application using 
-                       .NET framework
-                    </p>
-                  </div>
-                </div>
-              </div> 
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img src="assets/graphic designer.png" alt="graphic designer">
-                    <h4>Graphic Designing</h4>
-                  </div>
-                  <div class="flip-card-back">
-                    <p>
-                       I will do
-                       web and mobile UI design,
-                       logo design, business card 
-                       and stationery and social media
-                       design
-                    </p>
-                  </div>
-                </div>
-              </div> 
+      </div>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/app dev.png" alt="mobile app developer">
+            <h4>Mobile app development & design</h4>
+          </div>
+          <div class="flip-card-back">
+            <p>
+              I will develop cross platform
+              mobile application using
+              Flutter
+            </p>
+          </div>
         </div>
-        <h3 class="title" id="portfolio">Portfolio</h3>
-        <div class="portfolio">
-            <img src="assets/photo-grid.png" alt="portfolio">
+      </div>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/dev.png" alt="developer">
+            <h4>Desktop application development</h4>
+          </div>
+          <div class="flip-card-back">
+            <p>
+              I will develop desktop
+              application using
+              .NET framework
+            </p>
+          </div>
         </div>
-        <h3 class="title" id="about">About</h3>
-        <div class="about-description">
-            <div class="my-img">
-                <img src="assets/me.jpg" alt="">
-            </div>
-            <div class="about-container">
-                <p>
-                    Hiii, <br>
-                    I am Dushan Senadheera, I am a innovative, skillful undergraduate software engineer who follows BSc (Hons) Software Engineering degree program in NSBM Green University, Sri Lanaka that affiliates with University of Plymouth, UK. I am a person who loves to develop new skills and improve my current skills, my specialities are designing and developing web & mobile applications and I am a developing and designing enthusiast
-                </p>
-                <h3 class="title" id="contact">Get in touch</h3>
-                <div class="contact">
-                    <img src="assets/email.png" alt="">
-                    <img src="assets/facebook-logo-2019.png" alt="">
-                    <img src="assets/linkedin.png" alt="">
-                    <img src="assets/behance.png" alt="">
-                    <img src="assets/dribble.png" alt="">
-                </div>
-            </div>
+      </div>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/graphic designer.png" alt="graphic designer">
+            <h4>Graphic Designing</h4>
+          </div>
+          <div class="flip-card-back">
+            <p>
+              I will do
+              web and mobile UI design,
+              logo design, business card
+              and stationery and social media
+              design
+            </p>
+          </div>
         </div>
-    </main>
-    <footer>
-        <h3>Dushan Senadheera</h3>
-        <h6>Software Engineer | Digital Creator</h6>
-        <div class="social">
-            <img src="assets/email.png" alt="">
-            <img src="assets/facebook-logo-2019.png" alt="">
-            <img src="assets/linkedin.png" alt="">
-            <img src="assets/behance.png" alt="">
-            <img src="assets/dribble.png" alt="">
+      </div>
+    </div>
+    <h3 class="title" id="portfolio">Portfolio</h3>
+    <div class="portfolio">
+      <h5>Still on development</h5>
+      <!-- <div class="slideshow-container">
+        <div class="mySlides fade">
+          <div class="numbertext">1 / 3</div>
+          <img src="img1.jpg" style="width:100%">
+          <div class="text">Caption Text</div>
         </div>
-        <hr>
-        <small>&copy; Copyright 2022, Dushan Senadheera. All right reserved</small>
-    </footer>
-    <script src="script.js"></script> 
+
+        <div class="mySlides fade">
+          <div class="numbertext">2 / 3</div>
+          <img src="img2.jpg" style="width:100%">
+          <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">3 / 3</div>
+          <img src="img3.jpg" style="width:100%">
+          <div class="text">Caption Three</div>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      </div>
+      <br>
+
+      <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+      </div>
+    </div> -->
+    <h3 class="title" id="about">About</h3>
+    <div class="about-description">
+      <div class="my-img">
+        <img src="assets/me.jpg" alt="">
+      </div>
+      <div class="about-container">
+        <p>
+          Hiii, <br>
+          I am Dushan Senadheera, I am a innovative, skillful undergraduate software engineer who follows BSc (Hons)
+          Software Engineering degree program in NSBM Green University, Sri Lanaka that affiliates with University of
+          Plymouth, UK. I am a person who loves to develop new skills and improve my current skills, my specialities are
+          designing and developing web & mobile applications and I am a developing and designing enthusiast
+        </p>
+        <h3 class="title" id="contact">Get in touch</h3>
+        <div class="contact">
+          <img src="assets/email.png" alt="">
+          <img src="assets/facebook-logo-2019.png" alt="">
+          <img src="assets/linkedin.png" alt="">
+          <img src="assets/behance.png" alt="">
+          <img src="assets/dribble.png" alt="">
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <h3>Dushan Senadheera</h3>
+    <h6>Software Engineer | Digital Creator</h6>
+    <div class="social">
+      <img src="assets/email.png" alt="">
+      <img src="assets/facebook-logo-2019.png" alt="">
+      <img src="assets/linkedin.png" alt="">
+      <img src="assets/behance.png" alt="">
+      <img src="assets/dribble.png" alt="">
+    </div>
+    <hr>
+    <small>&copy; Copyright 2022, Dushan Senadheera. All right reserved</small>
+  </footer>
+  <script src="script.js"></script>
 </body>
+
 </html>
